c196df6b5a66e658e595fb9a74d965515cac82d4
Pepe Valdivia <jvaldivia@softweb.pe>
2017-12-07 16:53:46 -0500

    Primer archivo estático

--- config/contants.py
+++ config/contants.py
@@ -1,3 +1,5 @@
 constants = {
-
+  'BASE_URL' : 'http://localhost:3000/',
+  'STATIC_URL' : 'http://localhost:3000/static/',
+  'CSRF' : 'mpt/sr6eS2AlCRHU7DVThMgFTN08pnfSDf/C94eZx7udfm0lvgaYWLYJttYPKzGKDTlXwVU/d2FOxbKkgNlsTw==',
 }
