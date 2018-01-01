4b47a38b2b17967a259523cd0f69e497db23f730
Pepe Valdivia <jvaldivia@softweb.pe>
2018-01-01 08:44:46 -0500

    Listar los logs del usuario

--- accesos/usuario.py
+++ accesos/usuario.py
@@ -10,3 +10,8 @@ accesos_usuario = Blueprint('accesos_usuario', __name__)
 def listar():
   r = requests.get(constants['servicios']['accesos'] + 'usuario/listar')
   return r.text
+
+@accesos_usuario.route('/accesos/usuario/logs/<usuario_id>', methods=['GET'])
+def logs(usuario_id):
+  r = requests.get(constants['servicios']['accesos'] + 'usuario/listar_accesos/' + usuario_id)
+  return r.text
