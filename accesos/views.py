eed8a4ab3a9d9830e6961b6f5bfe0fd6d3dcda32
Pepe Valdivia <jvaldivia@softweb.pe>
2018-01-01 08:04:16 -0500

    Renderizado de vista de accesos

--- accesos/views.py
+++ accesos/views.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 import json
 from flask import Blueprint, request, render_template, redirect, session
+from config.constants import constants
 from config.middleware import csrf_form
 
 accesos = Blueprint('accesos', __name__)
 
-@login.route('/login', methods=['GET'])
+@accesos.route('/accesos', methods=['GET'])
 def index():
-	data = {'mensaje' : False}
-	return render_template('login/index.html', constants = constants, data = json.dumps(data))
+	menu = [
+		{'url' : 'accesos', 'nombre' : 'Accesos'},
+    {'url' : 'maestros', 'nombre' : 'Maestros'},
+		{'url' : 'agricultores', 'nombre' : 'Agricultores'},
+		{'url' : 'estaciones', 'nombre' : 'Estaciones'},
+  ]
+	items = [
+		{"subtitulo":"Opciones", "items":
+			[
+				{"item":"Gestión de Sistemas","url":"accesos/#/sistema"},
+				{"item":"Gestión de Usuarios","url":"accesos/#/usuario"}
+			]
+		},
+	]
+	data = {'titulo_pagina' : 'Gestión Accesos', 'modulo' : 'Accesos'}
+	return render_template('accesos/index.html', constants = constants, data = json.dumps(data), menu = json.dumps(menu), items = json.dumps(items))
