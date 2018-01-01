be46c7026d0f801ead9d3f95e6bd97f134916e98
Pepe Valdivia <jvaldivia@softweb.pe>
2017-12-31 20:55:09 -0500

    Quintado archivos estácticos

--- static/views/maestros/_form_distritos.js
+++
@@ -1,9 +0,0 @@
-var autocompleteDistritos = new Autocomplete();
-
-autocompleteDistritos.SetUrl(BASE_URL + "maestros/distrito/buscar?distrito=");
-autocompleteDistritos.SetUlSugerencia("autoDistrito");
-autocompleteDistritos.SetDestinoIdSugerencia("idDistrito");
-autocompleteDistritos.SetNombreObjeto("autocompleteDistritos");
-autocompleteDistritos.SetDestinoValorSugerencia("txtDistrito");
-autocompleteDistritos.SetIndices("id", "nombre");
-autocompleteDistritos.SetFuncionAdicional("");
